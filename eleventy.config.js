91eb5a03cbb2af04929a4155ff3164b3be133e90
jesarx <jesarx@proton.me>
2026-03-25 19:34:04 -0600

    arreglé config de eleventry

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,12 @@
 export default function (eleventyConfig) {
-  // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/css");
-
-  // Watch for CSS changes
   eleventyConfig.addWatchTarget("src/css/");
 
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "/var/www/433.mx",  // ← aquí
       includes: "_includes",
       data: "_data",
     },
